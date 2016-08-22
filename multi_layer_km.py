f1c65445235e7c2be10b87e346fc7a150e906b44
yang4173 <yang4173@7ca33a00-2d18-4fb8-bd27-5a10a10a3907>
2016-08-22 14:06:23 +0000

    write load-cifar, to allow working with cifar-10 dataset

--- multi_layer_km.py
+++ multi_layer_km.py
@@ -58,6 +58,128 @@ def sigmoid(x):
     
 class dA2(dA):
     # overload the original function in dA class
+    def __init__(
+            self,
+            numpy_rng,
+            theano_rng=None,
+            input=None,
+            n_visible=784,
+            n_hidden=500,
+            W=None,
+            bhid=None,
+            bvis=None
+        ):
+        """
+        Initialize the dA class by specifying the number of visible units (the
+        dimension d of the input ), the number of hidden units ( the dimension
+        d' of the latent or hidden space ) and the corruption level. The
+        constructor also receives symbolic variables for the input, weights and
+        bias. Such a symbolic variables are useful when, for example the input
+        is the result of some computations, or when weights are shared between
+        the dA and an MLP layer. When dealing with SdAs this always happens,
+        the dA on layer 2 gets as input the output of the dA on layer 1,
+        and the weights of the dA are used in the second stage of training
+        to construct an MLP.
+
+        :type numpy_rng: numpy.random.RandomState
+        :param numpy_rng: number random generator used to generate weights
+
+        :type theano_rng: theano.tensor.shared_randomstreams.RandomStreams
+        :param theano_rng: Theano random generator; if None is given one is
+                     generated based on a seed drawn from `rng`
+
+        :type input: theano.tensor.TensorType
+        :param input: a symbolic description of the input or None for
+                      standalone dA
+
+        :type n_visible: int
+        :param n_visible: number of visible units
+
+        :type n_hidden: int
+        :param n_hidden:  number of hidden units
+
+        :type W: theano.tensor.TensorType
+        :param W: Theano variable pointing to a set of weights that should be
+                  shared belong the dA and another architecture; if dA should
+                  be standalone set this to None
+
+        :type bhid: theano.tensor.TensorType
+        :param bhid: Theano variable pointing to a set of biases values (for
+                     hidden units) that should be shared belong dA and another
+                     architecture; if dA should be standalone set this to None
+
+        :type bvis: theano.tensor.TensorType
+        :param bvis: Theano variable pointing to a set of biases values (for
+                     visible units) that should be shared belong dA and another
+                     architecture; if dA should be standalone set this to None
+
+
+        """
+        self.n_visible = n_visible
+        self.n_hidden = n_hidden
+
+        # create a Theano random generator that gives symbolic random values
+        if not theano_rng:
+            theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
+
+        # note : W' was written as `W_prime` and b' as `b_prime`
+        if not W:
+            # W is initialized with `initial_W` which is uniformely sampled
+            # from -4*sqrt(6./(n_visible+n_hidden)) and
+            # 4*sqrt(6./(n_hidden+n_visible))the output of uniform if
+            # converted using asarray to dtype
+            # theano.config.floatX so that the code is runable on GPU
+            initial_W = numpy.asarray(
+                0.01*numpy.float32(numpy.random.randn(n_visible, n_hidden))               
+                
+            )
+#            initial_W = numpy.asarray(
+#                numpy_rng.uniform(
+#                    low=-4 * numpy.sqrt(6. / (n_hidden + n_visible)),
+#                    high=4 * numpy.sqrt(6. / (n_hidden + n_visible)),
+#                    size=(n_visible, n_hidden)
+#                ),
+#                dtype=theano.config.floatX
+#            )
+            W = theano.shared(value=initial_W, name='W', borrow=True)
+
+        if not bvis:
+            bvis = theano.shared(
+                value=numpy.zeros(
+                    n_visible,
+                    dtype=theano.config.floatX
+                ),
+                borrow=True
+            )
+
+        if not bhid:
+            bhid = theano.shared(
+                value=numpy.zeros(
+                    n_hidden,
+                    dtype=theano.config.floatX
+                ),
+                name='b',
+                borrow=True
+            )
+
+        self.W = W
+        # b corresponds to the bias of the hidden
+        self.b = bhid
+        # b_prime corresponds to the bias of the visible
+        self.b_prime = bvis
+        # tied weights, therefore W_prime is W transpose
+        self.W_prime = self.W.T
+        self.theano_rng = theano_rng
+        # if no input is given, generate a variable representing the input
+        if input is None:
+            # we use a matrix because we expect a minibatch of several
+            # examples, each example being a row
+            self.x = T.dmatrix(name='input')
+        else:
+            self.x = input
+
+        self.params = [self.W, self.b, self.b_prime]
+        
     def get_cost_updates(self, corruption_level, learning_rate):
         """ This function computes the cost and the updates for one trainng
         step of the dA """
@@ -68,9 +190,9 @@ class dA2(dA):
         # note : we sum over the size of a datapoint; if we are using
         #        minibatches, L will be a vector, with one entry per
         #        example in minibatch
-        L = - T.sum(self.x * T.log(z) + (1 - self.x) * T.log(1 - z), axis=1)
+#        L = - T.sum(self.x * T.log(z) + (1 - self.x) * T.log(1 - z), axis=1)
         
-#        L = T.sum(T.pow(self.x - z, 2), axis = 1)                
+        L = T.sum(T.pow(self.x - z, 2), axis = 1)                
         cost = T.mean(L)
 
         # compute the gradients of the cost of the `dA` with respect
@@ -541,7 +663,7 @@ def test_SdC(lbd = .01, finetune_lr= .005, mu = 0.9, pretraining_epochs=50,
     print '... pre-training the model'
     start_time = timeit.default_timer()
     ## Pre-train layer-wise
-    corruption_levels = [.1, .1, .1, .1, .1]
+    corruption_levels = [0, 0, 0, 0, 0]
     
     pretrain_lr_shared = theano.shared(numpy.asarray(pretrain_lr,
                                                    dtype='float32'),
@@ -758,14 +880,14 @@ def test_SdC(lbd = .01, finetune_lr= .005, mu = 0.9, pretraining_epochs=50,
 if __name__ == '__main__':      
     params = {'lbd': 0.005,               
               'pretraining_epochs': 30,
-              'pretrain_lr': .01, 
+              'pretrain_lr': .005, 
               'mu': 0.9,
               'finetune_lr': 0.001, 
               'training_epochs': 50,
               'dataset': 'mnist.pkl.gz', 
               'batch_size': 20, 
               'nClass': 4, 
-              'hidden_dim': [1000, 500, 250, 10]}
+              'hidden_dim': [1000]}
              
     test_SdC(**params)      
     
